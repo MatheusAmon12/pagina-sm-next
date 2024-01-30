c5fecd09872c8aa65b305c0f18ddf4a510bc128a
Matheus Amon <amonmatheus757@gmail.com>
2024-01-30 12:10:21 -0300

    Alterando cor do footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ import { useRouter } from 'next/router'
 const useStyles = makeStyles()((theme) => {
     return{
         container: {
-            background: theme.palette.primary.main,
+            background: theme.palette.secondary.main,
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
@@ -44,28 +44,28 @@ const Footer = () => {
     return(
         <Container maxWidth={`100vw`} className={classes.container}>
             <Box className={classes.socialIcons}>
-                <IconButton aria-label="whatsapp" color="secondary" onClick={() => handleRedirect('https://api.whatsapp.com/send?1=pt_BR&phone=553892622985')}>
+                <IconButton aria-label="whatsapp" color="primary" onClick={() => handleRedirect('https://api.whatsapp.com/send?1=pt_BR&phone=553892622985')}>
                     <WhatsAppIcon />
                 </IconButton>
 
-                <IconButton aria-label="istagram" color="secondary" onClick={() => handleRedirect('https://www.instagram.com/liviamelgaco.sm/')}>
+                <IconButton aria-label="istagram" color="primary" onClick={() => handleRedirect('https://www.instagram.com/liviamelgaco.sm/')}>
                     <InstagramIcon />
                 </IconButton>
             </Box>
 
             <Box className={classes.marginBox}>
                 <Button onClick={() => handleRedirect('https://wa.me/5538992622985/?text=Gostaria%20de%20saber%20mais%20sobre%20seus%20serviços?')}>
-                    <Typography component='span' variant='caption' color='secondary'>
+                    <Typography component='span' variant='caption' color='#F6F8FF'>
                         Informações
                     </Typography>
                 </Button>
                 <Button onClick={() => handleRedirect('https://wa.me/5538992622985/?text=Olá,%20estou%20precisando%20de%20ajuda.%20Pode%20me%20ajudar?')}>
-                    <Typography component='span' variant='caption' color='secondary' >
+                    <Typography component='span' variant='caption' color='#F6F8FF' >
                         Suporte
                     </Typography>
                 </Button>
                 <Button onClick={() => handleRedirect('/agency/contacts')}>
-                    <Typography component='span' variant='caption' color='secondary'>
+                    <Typography component='span' variant='caption' color='#F6F8FF'>
                         Contatos
                     </Typography>
                 </Button>
@@ -76,10 +76,10 @@ const Footer = () => {
                 <Typography 
                     component='span' 
                     variant='caption' 
-                    color='secondary' 
+                    color='#F6F8FF' 
                     style={{marginLeft: '2px'}}
                 >
-                    2024 Agência LM
+                    © 2024 Agência LM
                 </Typography>
             </Box>
 
