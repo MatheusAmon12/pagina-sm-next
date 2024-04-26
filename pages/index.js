4a0e4b1eb3f5841ead0ffbb802f682b0327a8db8
Matheus Amon <amonmatheus757@gmail.com>
2024-04-26 11:03:59 -0300

    refact(carousel images): abstrai o setup incial do carousel para uma funcao utilitaria

--- pages/index.js
+++ pages/index.js
@@ -8,25 +8,7 @@ import TemplateDefault from '../src/templates/Default'
 import Header from '../src/components/Header'
 import CardInfo from '../src/components/Card'
 import Carousel from '../src/components/Carousel'
-
-import carouselPosts_1 from '../public/images/carousel11.png'
-import carouselPosts_2 from '../public/images/carousel12.png'
-import carouselPosts_3 from '../public/images/carousel13.png'
-import carouselPosts_4 from '../public/images/carousel14.png'
-
-import carouselStories_1 from '../public/images/mockup-stories-1.jpg'
-import carouselStories_2 from '../public/images/mockup-stories-2.jpg'
-import carouselStories_3 from '../public/images/mockup-stories-3.jpg'
-import carouselStories_4 from '../public/images/mockup-stories-4.jpg'
-
-import carouselInsights_1 from '../public/images/insights1.jpg'
-import carouselInsights_2 from '../public/images/insights2.jpg'
-import carouselInsights_3 from '../public/images/insights3.jpg'
-import carouselInsights_4 from '../public/images/insights4.jpg'
-
-const imagesInsights = [carouselInsights_1, carouselInsights_2, carouselInsights_3, carouselInsights_4]
-const imagesStories = [carouselStories_1, carouselStories_2, carouselStories_3, carouselStories_4]
-const imagesFeed = [carouselPosts_1, carouselPosts_2, carouselPosts_3, carouselPosts_4]
+import { imagesFeed, imagesInsights, imagesStories } from '../src/utils/setupCarouselImages'
 
 const Home = () => {
   const { classes } = useStyles()
---
+++ src/utils/setupCarouselImages.js
@@ -0,0 +1,41 @@
+import carouselPosts_1 from '../../public/images/carousel11.png'
+import carouselPosts_2 from '../../public/images/carousel12.png'
+import carouselPosts_3 from '../../public/images/carousel13.png'
+import carouselPosts_4 from '../../public/images/carousel14.png'
+
+import carouselStories_1 from '../../public/images/mockup-stories-1.jpg'
+import carouselStories_2 from '../../public/images/mockup-stories-2.jpg'
+import carouselStories_3 from '../../public/images/mockup-stories-3.jpg'
+import carouselStories_4 from '../../public/images/mockup-stories-4.jpg'
+
+import carouselInsights_1 from '../../public/images/insights1.jpg'
+import carouselInsights_2 from '../../public/images/insights2.jpg'
+import carouselInsights_3 from '../../public/images/insights3.jpg'
+import carouselInsights_4 from '../../public/images/insights4.jpg'
+
+const imagesInsights = [
+    carouselInsights_1, 
+    carouselInsights_2, 
+    carouselInsights_3, 
+    carouselInsights_4
+]
+
+const imagesStories = [
+    carouselStories_1, 
+    carouselStories_2, 
+    carouselStories_3, 
+    carouselStories_4
+]
+
+const imagesFeed = [
+    carouselPosts_1, 
+    carouselPosts_2, 
+    carouselPosts_3, 
+    carouselPosts_4
+]
+
+export {
+    imagesFeed,
+    imagesInsights, 
+    imagesStories
+}
